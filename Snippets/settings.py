4b753a68ee8a9cd634e8c27c1d9ffe2e53393b3b
trds42 <trds42@gmail.com>
2022-11-25 11:35:16 +0300

    heroku

--- Snippets/settings.py
+++ Snippets/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -82,6 +85,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
